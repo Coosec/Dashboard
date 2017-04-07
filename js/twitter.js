bf4e8c84c6103fd27f6a74013dabd01433e0baaa
Jacob <jakub@kadzielawa.pl>
2017-04-08 00:11:12 +0200

    Drobne pooprawki css twitter

--- js/twitter.js
+++ js/twitter.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
-    const twitterSearch =  $('#btn-search');
 	const tweetsTemplate = $('#tweetsTemplate');
 	const inputSearch = $('#input-search');
-	
-	var myTmpl = $.templates("<li><div style='tweet_item'>Użytkownik:<b> {{:user.name}} </b> Liczba followersów: {{:user.followers_count}}<br/> Avatar: <img src='{{:user.profile_image_url}}'/></div><label>Treść:</label> {{:text}}</li>");
-	
-	twitterSearch.click(function() {
+	$('#btn-search').click(function() {
+		var countOftweets = parseInt($('#sel1 option:selected').text());
+
+		$('#searchResults').html('');
+		$('#preloader').show();
+		$('.search').css('opacity','0.1');
+
 		var value = inputSearch.val();
 		if(value !== ""){
-			$.post("twitter/tweets", { phrase: value }, function(data) {
-				var html = myTmpl.render(data);
-				console.log(data);
-				$("#searchResults").html(html);
-			});
+			setTimeout(showCode,2000,value,countOftweets);
 		}
 	});
 });
+
+function showCode(value,countOftweets){
+	var myTmpl = $.templates("<div class='tweet_item'>" +
+		"<div class='background'> <img src='{{:user.profile_background_image_url}}'/></div><div class='user'>" +
+		" <img src='{{:user.profile_image_url}}'/> <b> {{:user.name}}</b>" +
+		", followers: {{:user.followers_count}}<br/>" +
+		"</div><div class='description'><blockquote> {{:text}}<cite>{{:created_at}}</cite></blockquote></div></div>");
+
+
+	$.post("twitter/tweets", { phrase: value, countOfTweets: countOftweets  }, function(data) {
+		var html = myTmpl.render(data);
+		$("#searchResults").html(html);
+		$('#preloader').hide();
+		$('.search').css('opacity','1');
+
+
+	})};
