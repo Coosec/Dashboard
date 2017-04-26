2b7f0a69aacd3b29c78e7ecf317111610378eab9
Konrad Zapala <zapala.konrad@gmail.com>
2017-04-26 23:19:17 +0200

    Weather Api - debug session part 3

--- js/weather.js
+++ js/weather.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
     const weatherSearch =  $('#btn-search-weather');
 	const inputWeather = $('#input-search-weather');
 	
+	var myTmpl = $.templates("#weatherTemplate");
+
 	weatherSearch.click(function() {
 		var value = inputWeather.val();
 		if(value !== ""){
 			$.post("weather/address", { address: value }, function(data) {
 				console.log(data);
-				//var template = $.templates("<p><strong>Temperatura to: " + data.temperature + "</strong><p>");
-				//mapCanvas.html(template);
+				var html = myTmpl.render(data);
+				$("#weatherResult").html(html);
 			});
 		}
 	});
