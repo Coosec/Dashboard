2f4ae475d5e578778e1352c3e8569d0e172fc918
Paweł Wróblewski <Pawe Wrblewski>
2017-05-07 13:07:11 +0200

    zmiana prezentacji pogody

--- js/weather.js
+++ js/weather.js
@@ -4,11 +4,21 @@ $(document).ready(function() {
 	
 	var myTmpl = $.templates("#weatherTemplate");
 
+	function degToCompass(num) {
+		var val = Math.floor((num / 22.5) + 0.5);
+		var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
+		return arr[(val % 16)];
+	}
+	
 	weatherSearch.click(function() {
 		var value = inputWeather.val();
 		if(value !== ""){
 			$.post("weather/address", { address: value }, function(data) {
 				console.log(data);
+				data.wind.deg = degToCompass(data.wind.deg);
+				data.main.temp = (data.main.temp - 273).toFixed(2);
+				data.main.temp_max = (data.main.temp_max - 273).toFixed(2);
+				data.main.temp_min = (data.main.temp_min - 273).toFixed(2);
 				var html = myTmpl.render(data);
 				$("#weatherResult").html(html);
 			});
